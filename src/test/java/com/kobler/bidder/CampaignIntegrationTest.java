63c4c27460754f82ab6388d1a0edc74e8cb63942
deepakmhnp25 <deepakmhnp25@gmail.com>
2022-09-19 21:25:17 +0200

    test cases for campaign creation

--- src/test/java/com/kobler/bidder/CampaignIntegrationTest.java
+++ src/test/java/com/kobler/bidder/CampaignIntegrationTest.java
@@ -1,5 +1,7 @@
 package com.kobler.bidder;
 
+import com.kobler.bidder.domain.Campaign;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
@@ -7,7 +9,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
+import java.util.ArrayList;
+
+/**
+ * Integration test cases for Campaign related operations
+ */
 @SpringBootTest(classes = BidderApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class CampaignIntegrationTest {
@@ -25,14 +34,31 @@ public class CampaignIntegrationTest {
     @Test
     void testCampaignCreation(){
 
+        ResponseEntity<Campaign> campaignResponseEntity = this.testRestTemplate.postForEntity("http://localhost:" + port + "/campaigns", createCampaignRequest(), Campaign.class);
+
+        // status should be created
+        Assertions.assertEquals(campaignResponseEntity.getStatusCode(), HttpStatus.CREATED);
+        // should return the location id
+        Assertions.assertNotNull(campaignResponseEntity.getHeaders().getLocation());
+
     }
 
     /**
      * This should fail on empty campaign input values
      */
     @Test
-    void shouldFailOnEmptyInput(){
-
+    void shouldFailOnEmptyMandatoryInputParams(){
+        ResponseEntity<Campaign> campaignResponseEntity = this.testRestTemplate.postForEntity("http://localhost:" + port + "/campaigns", new Campaign(), Campaign.class);
+        Assertions.assertEquals(campaignResponseEntity.getStatusCode(), HttpStatus.BAD_REQUEST);
     }
 
+    private static Campaign createCampaignRequest() {
+        Campaign campaignRequest = new Campaign();
+        campaignRequest.setBudget(100);
+        campaignRequest.setName("Test");
+        ArrayList<String> keywords = new ArrayList<>();
+        keywords.add("kobler");
+        campaignRequest.setKeywords(keywords);
+        return campaignRequest;
+    }
 }
